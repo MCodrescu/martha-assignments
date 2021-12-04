17d66f8d519c4bcb9658a64dbda499c70faa99ea
MCodrescu <m.codrescu@outlook.com>
2021-12-04 16:59:16 -0600

    Fix issues connecting to MySQL

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'MCodrescu');
 define('DB_PASSWORD', 'Blackcar1997!');
-define('DB_NAME', 'assignments');
+define('DB_NAME', 'mysql');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
