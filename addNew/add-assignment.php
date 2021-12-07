8484cb1611e742f9b7d976781c9e0e761efc8848
MCodrescu <m.codrescu@outlook.com>
2021-12-07 17:48:46 -0600

    Create Add Assignment Page. Initial Setup

---
+++ addNew/add-assignment.php
@@ -0,0 +1,26 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: ..\Login\login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
